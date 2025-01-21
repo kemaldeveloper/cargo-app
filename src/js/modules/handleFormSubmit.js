95e6efb2af3b06ba83797fde7497dbd56eb078fc
frontDeveloper <gurbansahadowkemal01@gmail.com>
2025-01-21 18:04:31 +0300

    обработал ошибки в форме

--- src/js/modules/handleFormSubmit.js
+++ src/js/modules/handleFormSubmit.js
@@ -6,9 +6,19 @@ const cargoModal = new Modal(document.getElementById('addCargoModal'));
 
 export const handleFormSubmit = () => {
   const cargoForm = document.querySelector('#addCargoForm');
+  const errorContainer = document.querySelector('#formError');
+
   cargoForm.addEventListener('submit', e => {
     e.preventDefault();
     const form = e.target;
+
+    const hideError = () => {
+      errorContainer.textContent = '';
+      errorContainer.style.display = 'none';
+    };
+
+    cargoForm.addEventListener('input', hideError);
+    cargoForm.addEventListener('change', hideError);
 
     if (!form.checkValidity()) {
       form.classList.add('was-validated');
@@ -18,6 +28,18 @@ export const handleFormSubmit = () => {
 
     const statusSelect = form.status;
     const statusText = statusSelect.selectedOptions[0].textContent;
+    const departureDate = new Date(form.departureDate.value);
+    const currentDate = new Date();
+
+    currentDate.setHours(0, 0, 0, 0);
+
+    if (statusText === 'Доставлен' && departureDate > currentDate) {
+      errorContainer.textContent = 'Ошибка: Груз не может быть доставлен, если дата отправления в будущем.';
+      errorContainer.style.display = 'block';
+      form.classList.add('was-validated');
+      console.log('Ошибка: Дата отправления в будущем');
+      return;
+    }
 
     const newCargo = {
       id: `CARGO${String(Date.now()).slice(-6)}`,
